18b6c872901b94b57b0cb02a2d750b298779c0a9
Raymond Chan <raymond@9gag.com>
2018-05-29 16:51:01 +0800

    Test transparent

--- consent-library/src/main/java/com/google/ads/consent/ConsentForm.java
+++ consent-library/src/main/java/com/google/ads/consent/ConsentForm.java
@@ -77,6 +77,8 @@ public class ConsentForm {
         this.loadState = LoadState.NOT_READY;
 
         this.webView = new WebView(context);
+        this.webView.setBackgroundColor(Color.TRANSPARENT);
+        this.webView.setLayerType(WebView.LAYER_TYPE_SOFTWARE, null);
         this.dialog.setContentView(webView);
         this.dialog.setCancelable(false);
         webView.getSettings().setJavaScriptEnabled(true);
