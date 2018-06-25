720a88375ce6d4572584bbb7862440939a49ea53
Ram Parameswaran <rampara@google.com>
2018-06-25 23:46:19 +0100

    Version v1.0.5 of the Consent SDK.

--- consent-library/src/main/java/com/google/ads/consent/ConsentData.java
+++ consent-library/src/main/java/com/google/ads/consent/ConsentData.java
@@ -22,7 +22,7 @@ class ConsentData {
 
     private static final String SDK_PLATFORM = "android";
 
-    private static final String SDK_VERSION = "1.0.4";
+    private static final String SDK_VERSION = "1.0.5";
 
     @SerializedName("providers")
     private HashSet<AdProvider> adProviders;
